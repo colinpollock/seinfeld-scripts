fb98e6b63eed6da6bc83558278523e440d2df23d
Colin Pollock <colin@wordnik.com>
2012-02-26 20:35:08 -0800

    Adding word table and bigram view.

--- maketables.sql
+++ maketables.sql
@@ -32,7 +32,28 @@ CREATE TABLE sentence(
     FOREIGN KEY(utterance_id) REFERENCES utterance(id)
 );
 
-CREATE VIEW speaker_sentences AS
+
+CREATE TABLE word(
+    id INTEGER PRIMARY KEY,
+    sentence_id INTEGER NOT NULL,
+    word_number INTEGER NOT NULL,
+    text TEXT NOT NULL,
+    part_of_speech TEXT,
+    UNIQUE(sentence_id, word_number),
+    FOREIGN KEY(sentence_id) REFERENCES sentence(id)
+);
+
+CREATE VIEW bigram AS
+SELECT u.speaker, w1.text AS word_one, w2.text AS word_two, 
+       w1.part_of_speech AS pos_one, w2.part_of_speech AS pos_two
+FROM word w1 JOIN word w2 ON w1.sentence_id == w2.sentence_id AND 
+         w1.word_number == w2.word_number - 1
+     JOIN sentence s ON s.id == w1.sentence_id JOIN utterance u ON 
+         u.id == s.utterance_id
+;
+
+CREATE VIEW speaker_sentence AS
     SELECT u.speaker, s.text
     FROM utterance u JOIN sentence s ON u.id == s.utterance_id
 ;
+
